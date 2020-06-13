9277b3a54aa1d740281398db4cd505bbabfb70cc
Mike <mickael.merlet67@gmail.com>
2020-06-13 13:39:20 +0200

    Initial commit

---
+++ src/main/java/CameraCalibration.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+public class CameraCalibration extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // load the FXML resource
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("CC_FX.fxml"));
+            // store the root element so that the controllers can use it
+            BorderPane rootElement = (BorderPane) loader.load();
+            // set a whitesmoke background
+            rootElement.setStyle("-fx-background-color: whitesmoke;");
+            // create and style a scene
+            Scene scene = new Scene(rootElement, 800, 600);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            // create the stage with the given title and the previously created
+            // scene
+            primaryStage.setTitle("Camera Calibration");
+            primaryStage.setScene(scene);
+            // init the controller variables
+            CC_Controller controller = loader.getController();
+            controller.init();
+            // show the GUI
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // load the native OpenCV library
+       // System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        launch(args);
+    }
+}
